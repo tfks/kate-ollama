c29b031e69657f42005fe7f07a9b1fa44550bdfe
tfks <development@worldoflinux.nl>
2025-07-27 20:19:49 +0200

    Made everything work. Using a new type OllamaData to create the request data.

---
+++ src/ollamadata.cpp
@@ -0,0 +1,158 @@
+#include "ollamadata.h"
+
+#include <QJsonArray>
+
+OllamaData::OllamaData()
+    : context_(false)
+    , stream_(false)
+{
+}
+
+OllamaData::~OllamaData()
+{
+}
+
+void OllamaData::setModel(const QString &model)
+{
+    model_ = model;
+}
+QString OllamaData::getModel() const
+{
+    return model_;
+}
+
+void OllamaData::setPrompt(const QString &prompt)
+{
+    prompt_ = prompt;
+}
+QString OllamaData::getPrompt() const
+{
+    return prompt_;
+}
+
+void OllamaData::setSuffix(const QString &suffix)
+{
+    suffix_ = suffix;
+}
+QString OllamaData::getSuffix() const
+{
+    return suffix_;
+}
+
+void OllamaData::addImage(const QString &image)
+{
+    images_.append(image);
+}
+QVector<QString> OllamaData::getImages() const
+{
+    return images_;
+}
+
+void OllamaData::setFormat(const QString &format)
+{
+    format_ = format;
+}
+QString OllamaData::getFormat() const
+{
+    return format_;
+}
+
+void OllamaData::setOptions(const QString &options)
+{
+    options_ = options;
+}
+QString OllamaData::getOptions() const
+{
+    return options_;
+}
+
+void OllamaData::setSystem(const QString &system)
+{
+    system_ = system;
+}
+QString OllamaData::getSystem() const
+{
+    return system_;
+}
+
+void OllamaData::setContext(bool context)
+{
+    context_ = context;
+}
+bool OllamaData::getContext() const
+{
+    return context_;
+}
+
+void OllamaData::setStream(bool stream)
+{
+    stream_ = stream;
+}
+bool OllamaData::isStream() const
+{
+    return stream_;
+}
+
+void OllamaData::setRaw(bool raw)
+{
+    raw_ = raw;
+}
+bool OllamaData::isRaw() const
+{
+    return raw_;
+}
+
+void OllamaData::setKeepAlive(bool keepAlive)
+{
+    keepAlive_ = keepAlive;
+}
+bool OllamaData::isKeepAlive() const
+{
+    return keepAlive_;
+}
+
+QJsonObject OllamaData::toJson() const
+{
+    QJsonObject json;
+
+    if (!model_.isEmpty()) {
+        json.insert("model", QJsonValue(model_));
+    }
+    if (!prompt_.isEmpty()) {
+        json.insert("prompt", QJsonValue(prompt_));
+    }
+    if (!suffix_.isEmpty()) {
+        json.insert("suffix", QJsonValue(suffix_));
+    }
+    if (!format_.isEmpty()) {
+        json.insert("format", QJsonValue(format_));
+    }
+    if (!options_.isEmpty()) {
+        json.insert("options", QJsonValue(options_));
+    }
+    if (!system_.isEmpty()) {
+        json.insert("system", QJsonValue(system_));
+    }
+    if (context_) {
+        json.insert("context", QJsonValue(context_));
+    }
+    if (stream_) {
+        json.insert("stream", QJsonValue(stream_));
+    }
+    if (raw_) {
+        json.insert("raw", QJsonValue(raw_));
+    }
+    if (keepAlive_) {
+        json.insert("keep_alive", QJsonValue(keepAlive_));
+    }
+
+    QJsonArray imageArray;
+    for (const auto &image : images_) {
+        imageArray.append(QJsonValue(image));
+    }
+    if (!imageArray.isEmpty()) {
+        json.insert("images", imageArray);
+    }
+
+    return json;
+}
---
+++ src/ollamadata.h
@@ -0,0 +1,101 @@
+#ifndef OLLAMA_DATA_H
+#define OLLAMA_DATA_H
+
+#include <QJsonObject>
+#include <QString>
+#include <QVector>
+
+/*
+ * Can be used to create the request object to all Ollama with.
+ * Documentation: https://ollama.readthedocs.io/en/api/#generate-request-streaming
+ */
+class OllamaData
+{
+public:
+    // Default constructor
+    OllamaData();
+
+    // Default destructor
+    ~OllamaData();
+
+    // (required) Sets the model name
+    void setModel(const QString &model);
+    // (required) Gets the model name
+    QString getModel() const;
+
+    // Sets the prompt to generate a response for
+    void setPrompt(const QString &prompt);
+    // Gets the prompt to generate a response for
+    QString getPrompt() const;
+
+    // Sets the text after the model response
+    void setSuffix(const QString &suffix);
+    // Gets the text after the model response
+    QString getSuffix() const;
+
+    // Adds an image in Base64 format to an internal list which can be sent to Ollama
+    void addImage(const QString &image);
+    // Gets a list of images in Base64 format from an internal list
+    QVector<QString> getImages() const;
+
+    // Sets the format. The value json is only available option at the moment
+    void setFormat(const QString &format);
+    // Gets the format. The value json is only available option at the moment
+    QString getFormat() const;
+
+    // Sets additional model parameters listed in the documentation for the Modelfile such as temperature
+    // Documentation: https://ollama.readthedocs.io/en/modelfile/#valid-parameters-and-values
+    void setOptions(const QString &options);
+    // Gets additional model parameters listed in the documentation for the Modelfile such as temperature
+    // Documentation: https://ollama.readthedocs.io/en/modelfile/#valid-parameters-and-values
+    QString getOptions() const;
+
+    // Sets the system message (prompt) which is used.
+    // Set this system message (prompt) to (overrides what is defined in the Modelfile)
+    void setSystem(const QString &system);
+    // Gets the system message (prompt) which is used.
+    // Set this system message (prompt) to (overrides what is defined in the Modelfile)
+    QString getSystem() const;
+
+    // Sets the context parameter returned from a previous request to /generate, this can be used to keep a short conversation
+    void setContext(bool context);
+    // Gets the context parameter returned from a previous request to /generate, this can be used to keep a short conversation
+    bool getContext() const;
+
+    // Sets the is stream setting.
+    // If false the response will be returned as a single response object, rather than a stream of objects
+    void setStream(bool stream);
+    // Gets the is stream setting.
+    // If false the response will be returned as a single response object, rather than a stream of objects
+    bool isStream() const;
+
+    // Sets the is raw setting. If true no formatting will be applied to the prompt.
+    // You may choose to use the raw parameter if you are specifying a full templated prompt in your request to the API
+    void setRaw(bool raw);
+    // Gets the is raw setting. If true no formatting will be applied to the prompt.
+    // You may choose to use the raw parameter if you are specifying a full templated prompt in your request to the API
+    bool isRaw() const;
+
+    // Sets the keep alive setting. Default is 5m when not set.
+    void setKeepAlive(bool keepAlive);
+    // Gets the keep alive setting. Default is 5m when not set.
+    bool isKeepAlive() const;
+
+    // Converts all data, if filled, to a QJsonObject
+    QJsonObject toJson() const;
+
+private:
+    QString model_;
+    QString prompt_;
+    QString suffix_;
+    QVector<QString> images_;
+    QString format_;
+    QString options_;
+    QString system_;
+    bool context_;
+    bool stream_;
+    bool raw_;
+    bool keepAlive_;
+};
+
+#endif // OLLAMA_DATA_H
